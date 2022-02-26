a4d020425b81b8501a62cf9ff2f2cdbe80ca1c84
Vignesh-codezyng <vignesh@codezyng.com>
2022-02-26 12:31:37 +0530

    Added Scopes for youtube upload

--- Sources/OpenGoogleSignInSDK/Model/GoogleSignInScope.swift
+++ Sources/OpenGoogleSignInSDK/Model/GoogleSignInScope.swift
@@ -3,4 +3,7 @@ public enum GoogleSignInScope: String {
     case email
     case openID = "openid"
     case profile
+    case youTube = "https://www.googleapis.com/auth/youtube"
+    case YouTubeForceSsl = "https://www.googleapis.com/auth/youtube.force-ssl"
+    case YoutubeReadonly = "https://www.googleapis.com/auth/youtube.readonly"
 }
